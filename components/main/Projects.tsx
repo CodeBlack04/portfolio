e4ff5fe1ece4e41b7bdd66d2888b5348b4d7f6b8
CodeBlack04 <tauhidur.rahman1517@gmail.com>
2024-06-20 02:50:45 +0600

    edited project

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -4,7 +4,7 @@ import ProjectCard from '../sub/ProjectCard'
 const Projects = () => {
   return (
     <div id='projects' className='flex flex-col items-center justify-center py-20'>
-        <h1 className='text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20'>My Projects</h1>
+        <a href="http://165.232.181.102/" target='_blank'><h1 className='text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20 transition hover:scale-110'>My Projects</h1></a>
 
         <div className='h-full w-full flex flex-col md:flex-row gap-10 px-10'>
 
@@ -13,7 +13,7 @@ const Projects = () => {
             title="ShopMart - E-commerce Website"
             description="A website for online shopping that features real-time chat, a payment option (Stripe), and advanced filtering options."
             githubLink='https://github.com/CodeBlack04/ShopMart'
-            serverLink='http://165.232.179.107/'
+            serverLink='http://165.232.181.102/shopmart/'
             />
 
             <ProjectCard
@@ -21,6 +21,7 @@ const Projects = () => {
             title="BookMyHotel - Hotel Management Website"
             description="A website for hotel administration that includes live booking updates, chat help, staff management, and a rating system."
             githubLink='https://github.com/CodeBlack04/BookMyHotel'
+            serverLink='http://165.232.181.102/bookmyhotel/'
             />
 
             <ProjectCard
@@ -28,7 +29,7 @@ const Projects = () => {
             title="ExpenseTracker App"
             description="A system for tracking expenses that provides analysis and keeps record of all transactions."
             githubLink='https://github.com/CodeBlack04/ExpenseTracker'
-            serverLink='http://68.183.84.43/'
+            serverLink='http://165.232.181.102/expensetracker/'
             />
 
         </div>
